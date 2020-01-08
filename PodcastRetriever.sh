09e3dd8d5949bde6c7099f2f32d5fc97e388c1e7
Khepry Quixote <fracking.analysis@gmail.com>
2020-01-08 16:17:03 -0600

    Create PodcastRetriever.sh

---
+++ PodcastRetriever.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+
+# common shell emulators: xterm, rxvt, konsole, kvt, gnome-terminal, nxterm, eterm.
+
+terminal='xterm'
+
+tty -s || { ${terminal} -e $0 "$@"; exit 0; }
+
+./PodcastRetriever.py --keep  # put your requirements here
